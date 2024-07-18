6887f170a4ba03eb2412ef872d789fc5ab579e57
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-07-18 16:01:19 +0700

    test

--- test.js
+++ test.js
@@ -1,14 +1,29 @@
-const socket = new WebSocket('wss://stream.binance.com/stream');
+// const socket = new WebSocket('wss://stream.binance.com/stream');
 
-socket.addEventListener('open', function (event) {
-    console.log('Kết nối WSS đã được mở.');
+// socket.addEventListener('open', function (event) {
+//     console.log('Kết nối WSS đã được mở.');
 
-    socket.send(JSON.stringify({ id: 1,
-        method: "SUBSCRIBE",
-        params: ["!miniTicker@arr@3000ms"],
-     }));
+//     socket.send(JSON.stringify({ id: 1,
+//         method: "SUBSCRIBE",
+//         params: ["!miniTicker@arr@3000ms"],
+//      }));
+// });
+
+// socket.addEventListener('message', function (event) {
+//     console.log(JSON.parse(event.data));
+// });
+
+const Firestore = require('@google-cloud/firestore');
+
+const db = new Firestore({
+  projectId: 'alittledaisy',
+  keyFilename: './key.json',
 });
 
-socket.addEventListener('message', function (event) {
-    console.log(JSON.parse(event.data));
+const docRef = db.collection('users').doc('alovelace');
+
+await docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
 });
