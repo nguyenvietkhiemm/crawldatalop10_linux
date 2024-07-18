46d4b4ccc247c7b044e2a5f58a173b98549093f5
nguyenvietkhiemm <nguyenvietkhiemm@gmail.com>
2024-07-18 16:16:42 +0700

    hee

--- test.js
+++ test.js
@@ -1,18 +1,3 @@
-// const socket = new WebSocket('wss://stream.binance.com/stream');
-
-// socket.addEventListener('open', function (event) {
-//     console.log('Kết nối WSS đã được mở.');
-
-//     socket.send(JSON.stringify({ id: 1,
-//         method: "SUBSCRIBE",
-//         params: ["!miniTicker@arr@3000ms"],
-//      }));
-// });
-
-// socket.addEventListener('message', function (event) {
-//     console.log(JSON.parse(event.data));
-// });
-
 const Firestore = require('@google-cloud/firestore');
 
 const db = new Firestore({
@@ -33,3 +18,5 @@ async function writeData() {
         console.error('Error writing document: ', error);
     }
 }
+
+writeData();
